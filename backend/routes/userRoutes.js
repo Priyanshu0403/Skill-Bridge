dff3906ff62500767e887c51f90fdbc416c0c03e
Priyanshu Sarvaiyya <155910967+Priyanshu0403@users.noreply.github.com>
2026-01-09 17:19:17 +0530

    working on user routes

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,6 @@
+import express from "express";
+const router = express.Router();
+router.get('/', authMiddleware, getAllUsers);
+router.get('/:id', authMiddleware, getUserProfile);
+router.put("/:id", authMiddleware, updateUserProfile);
+export default router;
