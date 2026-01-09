79d80b31729eb255cd849eb05095a9fd853524f7
Priyanshu Sarvaiyya <155910967+Priyanshu0403@users.noreply.github.com>
2026-01-09 17:17:54 +0530

    Delete backend/routes/userRoutes.js

--- backend/routes/userRoutes.js
+++
@@ -1,6 +0,0 @@
-import express from "express";
-const router = express.Router();
-router.get('/', authMiddleware, getAllUsers);
-router.get('/:id', authMiddleware, getUserProfile);
-router.put("/:id", authMiddleware, updateUserProfile);
-export default router;
